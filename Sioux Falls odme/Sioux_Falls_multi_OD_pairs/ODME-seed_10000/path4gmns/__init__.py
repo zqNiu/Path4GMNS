cea1d17539b08d62e1c5408b2c9ce1a24eb80f98
zqNiu <19120879@bjtu.edu.cn>
2021-06-24 19:40:02 +0800

    add Sioux Falls test cases

---
+++ Sioux Falls odme/Sioux_Falls_multi_OD_pairs/ODME-seed_10000/path4gmns/__init__.py
@@ -0,0 +1,11 @@
+from .accessibility import *
+from .colgen import *
+from .dtaapi import *
+from .utils import *
+
+
+__version__ = '0.7.1'
+
+
+# print out the current version
+print('path4gmns, version '+__version__)
